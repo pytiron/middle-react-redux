c5e6533675bd3c917e4a5df2f37f8d833ce39129
unknown <dev.cslee@gmail.com>
2019-07-04 21:33:06 +0900

    [add] reducer_weather And promise add

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import Weather from './reducer_weather';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather : Weather
 });
 
 export default rootReducer;
