38a1687145d8d7564c1d08420edc3357e6cdb04e
unknown <dev.cslee@gmail.com>
2019-07-04 23:52:16 +0900

    [modify] graph package version update and graph apply

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,37 @@
 import React , { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component{
+
     renderWeather(cityData){
+        const name = cityData.city.name;
+        const temps = cityData.list.map(({main})=>main.temp);
+        const press = cityData.list.map(({main})=> main.pressure);
+        const humidity = cityData.list.map(({main})=> main.humidity);
+
         return(
             <tr key={cityData.city.id}>
                 <td>
-                    {cityData.city.name}
+                    {name}
                 </td>
                 <td>
-                    a
+                    <Sparklines
+                        data={temps} width={150} height={120}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
                 </td>
                 <td>
-                    b
+                    <Sparklines
+                        data={press} width={150} height={120}>
+                        <SparklinesLine color="green" />
+                    </Sparklines>
                 </td>
                 <td>
-                    c
+                    <Sparklines
+                        data={humidity} width={150} height={120}>
+                        <SparklinesLine color="blue" />
+                    </Sparklines>
                 </td>
             </tr>
         )
