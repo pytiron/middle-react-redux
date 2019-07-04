ff0d642f089f32b8fba4028f0d5f82d8ad4bc881
unknown <dev.cslee@gmail.com>
2019-07-04 21:16:44 +0900

    [modify] search ajax request action add

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 import { apiKey } from "../api_key";
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
-    const url = `${ROOT_URL}%q=${city},ko`;
+    const url = `${ROOT_URL}&q=${city}`;
     const request = axios.get(url);
 
     return {
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,10 @@
 import React , {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import { fetchWeather } from '../actions/index.js';
 
-export default class SearchBar extends Component{
+
+class SearchBar extends Component{
     constructor(props){
         super(props);
         this.state = {
@@ -8,11 +12,14 @@ export default class SearchBar extends Component{
         };
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     render() {
         return (
-            <form className="input-group">
+            <form
+                onSubmit={this.onFormSubmit}
+                className="input-group">
                 <input
                     placeholder="get a five-day forecast in your favorite cities"
                     className="form-control"
@@ -31,4 +38,16 @@ export default class SearchBar extends Component{
             term : event.target.value
         });
     }
+
+    onFormSubmit(event){
+        event.preventDefault();
+        this.props.fetchWeather(this.state.term);
+        this.setState({term:''});
+    }
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
