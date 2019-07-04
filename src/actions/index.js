0b998ef1f684feede49be7cbfe7841d105da2a91
unknown <dev.cslee@gmail.com>
2019-07-04 20:22:37 +0900

    [modify] import error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import axios from 'axis';
+import axios from 'axios';
 
 import { apiKey } from "../api_key";
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function weatherSearch(city){
+export function fetchWeather(city){
     const url = `${ROOT_URL}%q=${city},ko`;
     const request = axios.get(url);
 
